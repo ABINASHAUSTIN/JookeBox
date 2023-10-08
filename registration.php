5d33ee5a0c16710354371c73f04133efaeb73747
Abinash M <115488887+ABINASHAUSTIN@users.noreply.github.com>
2023-10-08 20:46:50 +0530

    Update registration.php

--- registration.php
+++ registration.php
@@ -6,16 +6,24 @@
 <body>
     <h2>Register</h2>
     <form method="POST" action="register_process.php">
+        <pre>
         <label for="username">Username:</label>
-        <input type="text" name="username" required><br>
+        <input type="text" name="username" required>
 
         <label for="password">Password:</label>
-        <input type="password" name="password" required><br>
+        <input type="password" name="password" required>
 
         <label for="email">Email:</label>
-        <input type="email" name="email" required><br>
+        <input type="email" name="email" required>
+
+        <label for="phone number">Phone Number:</label>
+        <input type="number" name="phone number" required>
+
+        <label for="address">Address:</label>
+        <input type="text" name="address" required>
 
         <input type="submit" value="Register">
+        </pre>
     </form>
 </body>
 </html>
